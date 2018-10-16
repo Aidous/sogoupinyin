7b001a8d816e3a0cc427253e6130f631f1359030
Aidous <2220170232@bit.edu.cn>
2018-10-16 21:24:23 +0800

    Update install_sougou.bash

--- install_sougou.bash
+++ install_sougou.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "ready install sogoupinyin for ubuntu 16.04."
-sudo sed -i '$a\deb http://archive.ubuntukylin.com:10006/ubuntukylin trusty main' /etc/apt/sources.list
+sudo sed -i '$a\deb http://archive.ubuntukylin.com:10006/ubuntukylin xenial main' /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install sogoupinyin 
 echo "sogoupinyin has been installed in your computer"

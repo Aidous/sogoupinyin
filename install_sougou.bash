3a731188bce210aa31160c33d1d0cbce41a43bb1
Aidous <2220170232@bit.edu.cn>
2018-11-12 11:37:09 +0800

    update

--- install_sougou.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "ready install sogoupinyin for ubuntu 16.04."
-sudo sed -i '$a\deb http://archive.ubuntukylin.com:10006/ubuntukylin xenial main' /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install sogoupinyin 
-echo "sogoupinyin has been installed in your computer"
-echo "if you can't find it,you need to restart your computer"
